245c428ab0f49ce3165cf462494adc58c58daea8
K_Kwon <ts04031@gmail.com>
2022-01-30 06:15:30 +0900

    firebase연결

--- app/src/main/java/com/example/stagram/LoginActivity.kt
+++ app/src/main/java/com/example/stagram/LoginActivity.kt
@@ -2,10 +2,13 @@ package com.example.stagram
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
+    var auth : FirebaseAuth? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        auth = FirebaseAuth.getInstance()
     }
 }
