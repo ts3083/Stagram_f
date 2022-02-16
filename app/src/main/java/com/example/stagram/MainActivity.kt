f921166708efdaea9fd0d791408e27e9d1c994a4
K_Kwon <ts04031@gmail.com>
2022-02-16 19:27:31 +0900

    사진 업로드 페이지  - 갤러리에서 사진이 선택되지 않는 오류 해결

--- app/src/main/java/com/example/stagram/MainActivity.kt
+++ app/src/main/java/com/example/stagram/MainActivity.kt
@@ -5,16 +5,19 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
 import com.example.stagram.databinding.ActivityMainBinding
 import com.example.stagram.navigation.*
-import com.google.firebase.auth.FirebaseUser
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
+    private var launcer = registerForActivityResult(ActivityResultContracts.GetContent()) {
+        it -> changeFragment(GalleryFragment(it))
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,11 +44,15 @@ class MainActivity : AppCompatActivity() {
                         changeFragment(GridFragment())
                     }
                     R.id.action_add_photo -> {
-                        if(ContextCompat.checkSelfPermission(this@MainActivity, Manifest.permission.READ_EXTERNAL_STORAGE)
+                        if(ContextCompat.checkSelfPermission(this@MainActivity.applicationContext,
+                                android.Manifest.permission.READ_EXTERNAL_STORAGE)
                             == PackageManager.PERMISSION_GRANTED) {
-                            goAddPhotoActivity()
+                            launcer.launch("image/*")
+                        } else {
+                            Toast.makeText(this@MainActivity,
+                                "갤러리 접근 권한이 거부되어 있습니다. 설정에서 접근을 허용해 주세요.",
+                                Toast.LENGTH_SHORT).show()
                         }
-
                     }
                     R.id.action_favorite_alarm -> {
                         changeFragment(AlarmFragment())
@@ -67,9 +74,9 @@ class MainActivity : AppCompatActivity() {
             .commit()
     }
 
-    fun goAddPhotoActivity() {
+    /*fun goAddPhotoActivity() {
         startActivity(Intent(this, AddPhotoActivity::class.java)) // 메인 액티비티 호출
-    }
+    }*/
 }
 
 
---
+++ app/src/main/java/com/example/stagram/navigation/GalleryFragment.kt
@@ -0,0 +1,50 @@
+package com.example.stagram.navigation
+
+import android.net.Uri
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.example.stagram.R
+import com.example.stagram.databinding.FragmentGalleryBinding
+import com.google.firebase.storage.FirebaseStorage
+import java.text.SimpleDateFormat
+import java.util.*
+
+
+class GalleryFragment(uri: Uri) : Fragment() {
+    private lateinit var mBinding : FragmentGalleryBinding
+    private var uri : Uri? = uri
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        mBinding = FragmentGalleryBinding.inflate(inflater, container, false)
+
+        mBinding.addphotoImage.setImageURI(uri)
+        mBinding.addphotoBtnUpload.setOnClickListener {
+            uploadImageOFFirebase(uri!!)
+        }
+        /*val view = LayoutInflater.from(activity).inflate(R.layout.fragment_gallery,
+            container, false)*/
+        return mBinding.root
+    }
+
+    fun uploadImageOFFirebase(uri: Uri) {
+        var storage : FirebaseStorage? = FirebaseStorage.getInstance()
+        var fileName = "IMAGE_${SimpleDateFormat("yyyymmdd_HHmmss").format(Date())}_.png"
+
+        // 파일 로드, 다운로드, 삭제, 메타데이터 가져오기 또는 업데이트를 하기 위해 레퍼런스 생성
+        // 참조는 클라우드 파일을 가리키는 포인터라고 할 수 있음
+        var imageRef = storage!!.reference.child("image/").child(fileName)
+
+        imageRef.putFile(uri!!).addOnSuccessListener {
+            Toast.makeText(activity, getString(R.string.upload_success), Toast.LENGTH_LONG).show()
+        }.addOnFailureListener {
+            println(it)
+            Toast.makeText(activity, getString(R.string.upload_fail), Toast.LENGTH_SHORT).show()
+
+        }
+    }
+}
